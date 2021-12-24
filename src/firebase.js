38ad809fa9db2321ec102ccf1707420f41d89491
Mayur-Arde <mayurarde8@gmail.com>
2021-12-24 15:43:23 +0530

    boilerplate

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCsLR6_PwU4ZYPlt0ZIxZmuHyC6ddLR49c",
+  authDomain: "netflix-clone-mayur-arde.firebaseapp.com",
+  projectId: "netflix-clone-mayur-arde",
+  storageBucket: "netflix-clone-mayur-arde.appspot.com",
+  messagingSenderId: "634241137711",
+  appId: "1:634241137711:web:cf0fb8c30b72d5f22adada"
+};
