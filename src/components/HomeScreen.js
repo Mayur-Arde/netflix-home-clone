579901ed73e9ca4227557e6d2d0e9e5ca179d3a3
Mayur-Arde <mayurarde8@gmail.com>
2021-12-25 17:28:27 +0530

    firebase deploy

--- src/components/HomeScreen.js
+++ src/components/HomeScreen.js
@@ -5,22 +5,51 @@ import Row from './Row';
 import request from '../api call/request';
 function HomeScreen() {
   return (
-    <div className='homeScreen'>
+    <div className="homeScreen">
       {/* navbar */}
       <Nav></Nav>
       {/* hero  */}
       <Header></Header>
       {/* bunch of rows */}
-      <Row title={'NETFLIX ORIGINALS'} fetchURL={request.fetchNetflixOriginals} isLongPoster></Row>
-      <Row title={'Trending Now'} fetchURL={request.fetchTrending}></Row>
-      <Row title={'Top Rated'} fetchURL={request.fetchTopRated}></Row>
-      <Row title={'Action Movies'} fetchURL={request.fetchActionMovies}></Row>
-      <Row title={'Comedy Movies'} fetchURL={request.fetchComedyMovies}></Row>
-      <Row title={'Horror Movies'} fetchURL={request.fetchHorrorMovies}></Row>
-      <Row title={'Romance Movies'} fetchURL={request.fetchRomanceMovies}></Row>
-      <Row title={'Documentaries'} fetchURL={request.fetchDocumentaries}></Row>
+      <Row
+        title={'NETFLIX ORIGINALS'}
+        fetchURL={request.fetchNetflixOriginals}
+        isLongPoster
+        id={'NO'}
+      ></Row>
+      <Row
+        title={'Trending Now'}
+        fetchURL={request.fetchTrending}
+        id="TN"
+      ></Row>
+      <Row title={'Top Rated'} fetchURL={request.fetchTopRated} id={'TR'}></Row>
+      <Row
+        title={'Action Movies'}
+        fetchURL={request.fetchActionMovies}
+        id={'AM'}
+      ></Row>
+      <Row
+        title={'Comedy Movies'}
+        fetchURL={request.fetchComedyMovies}
+        id={'CM'}
+      ></Row>
+      <Row
+        title={'Horror Movies'}
+        fetchURL={request.fetchHorrorMovies}
+        id={'HM'}
+      ></Row>
+      <Row
+        title={'Romance Movies'}
+        fetchURL={request.fetchRomanceMovies}
+        id={'RM'}
+      ></Row>
+      <Row
+        title={'Documentaries'}
+        fetchURL={request.fetchDocumentaries}
+        id={'D'}
+      ></Row>
     </div>
-  )
+  );
 }
 
-export default HomeScreen
+export default HomeScreen;
--- src/components/Row.js
+++ src/components/Row.js
@@ -2,7 +2,9 @@ import React, { useState, useEffect } from 'react';
 import './Row.css';
 import axios from '../api call/axios';
 
-function Row({ title, fetchURL, isLongPoster }) {
+import { FaArrowLeft } from 'react-icons/fa';
+import { FaArrowRight } from 'react-icons/fa';
+function Row({ title, fetchURL, isLongPoster, id }) {
   const [movies, setMovies] = useState([]);
 
   const fetchApi = async () => {
@@ -17,22 +19,42 @@ function Row({ title, fetchURL, isLongPoster }) {
   const baseURL = 'https://image.tmdb.org/t/p/original/';
   return (
     <div className="row">
-      <h2 className="row__title">{title}</h2>
-      <div className="row__posters">
-        {movies.map((movie) => {
-          return (
-            ((isLongPoster && movie.poster_path) ||
-              (!isLongPoster && movie.backdrop_path)) && (
-              <img
+      <h2>{title}</h2>
+      <div class="slider">
+        <div className=" arrow arrow__left">
+          <span
+            className="icon__left"
+            onClick={() => {
+              document.getElementById(id).scrollLeft -= window.innerWidth - 80;
+            }}
+          >
+            <FaArrowLeft />
+          </span>
+        </div>
+        <div id={id} className="row__posters">
+          {/**SEVERAL ROW__POSTER */}
+          {movies.map((movie) => (
+            <img
+              key={movie.id}
               className={`rowPoster ${isLongPoster && 'rowLongPoster'}`}
-                src={`${baseURL}${
-                  isLongPoster ? movie.poster_path : movie.backdrop_path
-                }`}
-                alt="movie"
-              />
-            )
-          );
-        })}
+              src={`${baseURL}${
+                isLongPoster ? movie.poster_path : movie.backdrop_path
+              }`}
+              loading="lazy"
+              alt={movie.name}
+            />
+          ))}
+        </div>
+        <div className="arrow arrow__right">
+          <span
+            className="icon__right"
+            onClick={() => {
+              document.getElementById(id).scrollLeft += window.innerWidth - 80;
+            }}
+          >
+            <FaArrowRight />
+          </span>
+        </div>
       </div>
     </div>
   );
